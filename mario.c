90e0a3a80642ed3ce0220ac658a3086d50804f6e
jeevesh2002 <jeevesh2002@users.noreply.github.com>
2020-10-09 05:30:35 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -3,22 +3,26 @@
 
 int main(void)
 {
-int n;
-do
-{
-    n=get_int("Height: ");                 
-}
-while (n<1||n>8);
-
-for (int i=0;i<n;i++)
+ int n;
+ do
   {
-      for (int j=0;j<n;j++)
-      {
-          if(i+j<n-1)
-          printf(" ");
-          else
-          printf("#");
-      }
+      n=get_int("Height: ");                   //prompts user for a height
+  }                                           
+  while (n < 1 || n > 8);                      //reprompts user for height if it is not in between 1 or 8 inclusive
+
+  for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)        
+       {
+          if (i + j < n - 1)
+           {                                   // prints spaces bfore printing #
+               printf(" ");               
+           }          
+          else                                 //number of spaces + # = height
+           {
+               printf("#");
+           }      
+       }
       printf("\n");
-  }  
+    }  
 }
