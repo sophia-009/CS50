3358154f35ea9c94ee3a08d01b10494c43f95474
jeevesh2002 <jeevesh2002@users.noreply.github.com>
2020-10-10 18:11:20 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -1,44 +1,74 @@
-#include <stdio.h>
-#include <cs50.h>
-
+# include <cs50.h>
+# include <math.h>
+# include <stdio.h>
 int main(void)
 {
-    long cc_number = get_long("Number: ");
-    int digit1 = 0, digit2 = 0, num_digits = 0, sum_of_double_odds = 0, sum_of_evens = 0;
-
-    while (cc_number > 0)
+    //finding number of digits in a number
+    int count = 0;
+    long cc_no ;
+    cc_no = get_long("enter cc_no: ");
+    long temp = cc_no;
+    while (temp > 0)
     {
-        digit2 = digit1;
-        digit1 = cc_number % 10;
+        count++;
+        temp = temp / 10;
+    }
 
-        if (num_digits % 2 == 0)
+    long cc_nox = cc_no ;
+    int tempcc ;
+    int last = 0 ;
+    int secTolast = 0 ;
+    while (cc_no > 0)
+    {
+        last += cc_no % 10 ;
+
+        cc_no /= 10;
+        tempcc = (cc_no % 10) * 2 ;
+        if (tempcc > 9)
         {
-            sum_of_evens += digit1;
+            secTolast += (tempcc % 10) + ((tempcc / 10) % 10);
         }
         else
         {
-            int multiple = 2 * digit1;
-            sum_of_double_odds += (multiple / 10) + (multiple % 10);
+            secTolast += tempcc ;
         }
-
-        cc_number /= 10;
-        num_digits++;
+        cc_no /= 10 ;
     }
-
-    bool is_valid = (sum_of_evens + sum_of_double_odds) % 10 == 0;
-    int first_two_digits = (digit1 * 10) + digit2;
-
-    if (digit1 == 4 && num_digits >= 13 && num_digits <= 16 && is_valid)
+    if ((last + secTolast) % 10 == 0)
     {
-        printf("VISA\n");
-    }
-    else if (first_two_digits >= 51 && first_two_digits <= 55 && num_digits == 16 && is_valid)
-    {
-        printf("MASTERCARD\n");
-    }
-    else if ((first_two_digits == 34 || first_two_digits == 37) && num_digits == 15 && is_valid)
-    {
-        printf("AMEX\n");
+        if (count == 15)
+        {
+            int tempA = cc_nox / 10000000000000;
+            if (tempA == 34 || tempA == 37)
+            {
+                printf("AMEX\n");
+            }
+        }
+        else if (count == 16)
+        {
+            int tempM = cc_nox / 100000000000000;
+            if (tempM == 51 || tempM == 52 || tempM == 53 || tempM == 54 || tempM == 55)
+            {
+                printf("MASTERCARD\n");
+            }
+        }
+        else if (count == 13)
+        {
+            int tempV1 = cc_nox / 1000000000000;
+            if (tempV1 == 4)
+            {
+                printf("VISA\n");
+            }    
+        }
+        
+        else if (count == 16)
+        {
+            int tempV2 = cc_nox / 1000000000000000;
+            if (tempV2 == 4)
+            {
+                printf("VISA\n");
+            }    
+        }
     }
     else
     {
